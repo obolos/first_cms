a3763ddf4112141366eeb31ed193c1b34c0e3fd6
Obolos <mycontactperson@yandex.ru>
2017-09-16 16:25:20 +0300

    refactoring

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!-- Header -->
+
+<?php
+
+include "includes/header.php";
+
+?>
+
+    <!-- Navigation -->
+<?php
+    include("includes/navigation.php");
+?>
+
+    <!-- Page Content -->
+
+            <?php include("includes/blog.php"); ?>
+
+
+
+    <!-- Blog Sidebar Widgets Column -->
+            <?php include("includes/sidebar.php") ?>
+
+    <!-- Footer -->
+
+<?php
+
+include("includes/footer.php");
+
+?>
